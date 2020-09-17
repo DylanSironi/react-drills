f393dc5f29f1f4645da14621c0de94ce4685d013
DylanSironi <sironidylan@gmail.com>
2020-09-17 17:35:58 -0500

    question-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Todo from './Todo'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: [],
+      box: "",
+    };
+    this.addItem = this.addItem.bind(this)
+  }
+  func1(value) {
+    this.setState({ box: value });
+  }
+  addItem() {
+    this.setState({
+      list: [...this.state.list, this.state.box],
+      box: ""
+    });
+  }
+  render() {
+    let list = this.state.list.map((element, index) => {
+      return <Todo key={index} task={element} />;
+    });
+    return (
+      <div className="App">
+        <h1>My to-do list:</h1>
+        <div>
+          <box
+            value={this.state.box}
+            placeholder="Enter new task"
+            onChange={e => this.func1(e.target.value)}
+          />
+          <button onClick={this.addItem}>Add</button>
+        </div>
+        <br />
+        {list}
+      </div>
+    );
+  }
 }
-
 export default App;
